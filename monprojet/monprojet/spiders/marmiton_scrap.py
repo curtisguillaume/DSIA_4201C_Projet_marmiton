409e687a0c3f5b4b4613a82f33580e369405d292
vlad0sn <vladsenechal@gmail.com>
2025-01-09 15:45:44 +0100

    ajout etapes, topcom

--- monprojet/monprojet/spiders/marmiton_scrap.py
+++ monprojet/monprojet/spiders/marmiton_scrap.py
@@ -4,7 +4,7 @@ from scrapy.http import Request
 class MarmitonSpider(scrapy.Spider):
     name = 'marmit'
     allowed_domains = ['marmiton.org']
-    start_urls = ['https://www.marmiton.org/recettes/index/categorie/aperitif-ou-buffet/']
+    start_urls = ['https://www.marmiton.org/recettes/']
 
     # Custom settings for this spider
     custom_settings = {
@@ -20,19 +20,20 @@ class MarmitonSpider(scrapy.Spider):
 
     def parse(self, response):
         # Extraire les liens de recettes à partir de la page actuelle
-        recettes = response.css("#content .mrtn-card__title a")
-        
+        recettes = response.css("#content .mrtn-card__title a::attr(href)").getall() or response.css(".recipe-card-link::attr(href)").getall()
+    
         for recette in recettes:
-            lien = response.urljoin(recette.css("::attr(href)").get())
+            lien = response.urljoin(recette)
             yield Request(url=lien, callback=self.parse_recipe, meta={'lien': lien})
-        
+    
         # Extraire tous les liens de numéros de page
         page_links = response.css('nav.af-pagination a::attr(href)').extract()
-        
+    
         # Suivre tous les liens vers les pages suivantes, sauf la page actuelle (en évitant les duplications)
         for page_link in page_links:
-            # Scrapy va suivre les liens vers les pages suivantes (par exemple, page 2, page 3, etc.)
+        # Scrapy va suivre les liens vers les pages suivantes (par exemple, page 2, page 3, etc.)
             yield Request(url=response.urljoin(page_link), callback=self.parse)
+
     
     def parse_recipe(self, response):
         # Extraire le titre de la recette et la difficulté
@@ -57,6 +58,18 @@ class MarmitonSpider(scrapy.Spider):
             ingredients.append(ingredient_text)
         
 
+        ustenciles = response.css(".card-utensil-quantity::text").getall()
+        cleaned_utensils = [" ".join(item.split()) for item in ustenciles]
+        etapes=response.css('.recipe-step-list__container p::text').getall() #mettre des 1. 2. 3. pour chaque étape?
+        topcom = response.css(".recipe-reviews-list__review__text::text").get()
+        topcom = topcom.strip() if topcom else None
+
+        #rajouter la catéégorie de la recette?? aperetif, deujeuner, sucre, salé, etc
+
+
+        
+        
+
         yield {
             "lien": lien,
             "titre": titre,
@@ -64,6 +77,9 @@ class MarmitonSpider(scrapy.Spider):
             "note": note,
             "temps": temps,
             "prix": prix,
-            "ingredients": ingredients 
+            "ingredients": ingredients ,
+            "ustenciles": cleaned_utensils,
+            "etapes": etapes, 
+            "topcom": topcom
 
         }

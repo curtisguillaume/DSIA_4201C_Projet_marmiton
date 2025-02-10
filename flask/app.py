e5cb5ea50646ca27802de9d52b75a932360770bd
vlad0sn <vladsenechal@gmail.com>
2025-02-10 12:45:13 +0100

    style

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 from elasticsearch import Elasticsearch
-
 import time
 
 app = Flask(__name__)
@@ -16,20 +15,40 @@ INDEX_NAME = "recettes"
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        search_term = request.form['search'] #recupere la valeur du champs de recherche
-        body = { #requete de recherche elastic search
+        search_term = request.form['search']
+        difficulte = request.form.get('difficulte', '').strip().lower()
+        prix = request.form.get('prix', '').strip().lower()
+
+        # Construction de la requête Elasticsearch
+        body = {
             "query": {
-                "match": {
-                    "titre": {
-                        "query": search_term,
-                        "fuzziness": "AUTO"
-            }
+                "bool": {
+                    "must": [
+                        {
+                            "match": {
+                                "titre": {
+                                    "query": search_term,
+                                    "fuzziness": "AUTO"
+                                }
+                            }
+                        }
+                    ],
+                    "filter": []
                 }
             }
         }
-        results = es.search(index=INDEX_NAME, body=body) #envoie de la requete
+
+        # Ajout des filtres si ils sont spécifiés
+        if difficulte:
+            body["query"]["bool"]["filter"].append({"term": {"difficulte.keyword": difficulte}})
+        if prix:
+            body["query"]["bool"]["filter"].append({"term": {"prix.keyword": prix}})
+
+        print(f"Requête Elasticsearch: {body}")  # Ajoutez cette ligne pour déboguer
+
+        results = es.search(index=INDEX_NAME, body=body)
         return render_template('index.html', results=results['hits']['hits'])
-    
+
     return render_template('index.html')
 
 @app.route('/recette/<id>')
